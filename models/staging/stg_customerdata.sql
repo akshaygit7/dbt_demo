21a597fb83122c9901858ae4f36b07f94cce812f
Akshay Angadi <akshayangadi7@gmail.com>
2024-12-28 11:32:31 +0000

    commit

--- models/staging/stg_customerdata.sql
+++ models/staging/stg_customerdata.sql
@@ -5,5 +5,5 @@ WITH tb1 as(
     id ,
     first_name,
     last_name
-    from {{source('datafeed_shared_schema','stg_customerdata')}})
+    from {{source('datafeed_shared_schema','raw_customerdata')}})
 select * from tb1
